92c63833ae3ab69da94f33ba0bcf515188d579c0
Paul Hatte <paul.hatte@gmail.com>
2024-11-01 10:50:00 +0100

    initialisation du frontend

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch('whatyearisit-backend-one-phi.vercel.app/year')
+    .then(result => result.json())
+    .then(data => {
+        console.log();
+        document.querySelector('#year').textContent = data.year;
+    })
+    .catch(error => console.error(error));
